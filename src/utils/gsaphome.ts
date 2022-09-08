702f1d33673a51d7d96aa600119d0e0927d01109
adammuflihun <96274551+adammuflihun@users.noreply.github.com>
2022-09-08 11:25:19 +0700

    s

--- src/utils/gsaphome.ts
+++ src/utils/gsaphome.ts
@@ -2,14 +2,29 @@ import gsap from 'gsap';
 
 export const gsaphome = () => {
   console.log(`ancol!`);
-  gsap.from($('.dot'), {
-    opacity: 0,
-    duration: 2,
-    repeat: -1,
-    ease: 'Bounce.easeIn',
-    stagger: {
-      from: 'random',
-      amount: 0.5,
-    },
-  });
+
+  const tlHomeHeader = gsap.timeline();
+
+  // gsap home header
+  let targets = $('.dot');
+  let number0fTargets = targets.length;
+  let duration = 0.1;
+  let repeatDelay = duration * (number0fTargets - 1);
+
+  tlHomeHeader
+    .from($('.dot'), {
+      opacity: 0,
+      duration: 2,
+      yoyo: true,
+      repeat: -1,
+      ease: 'Bounce.easeIn',
+      stagger: {
+        from: 'random',
+        each: duration,
+        repeat: -1,
+        repeatDelay: repeatDelay,
+      },
+    })
+    .from('.graphic-box', { y: 150, opacity: 0 }, 0.5)
+    .to('.graphic-box', { duration: 2, yoyo: 1, y: 50, repeat: -1, ease: 'Power1.easeInOut' }, 1);
 };
