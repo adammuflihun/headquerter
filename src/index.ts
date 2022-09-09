4b799d4e183131046a7ffd2f26fb5f778d64d0ce
adammuflihun <96274551+adammuflihun@users.noreply.github.com>
2022-09-09 11:36:01 +0700

    s

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import { gsaphome } from '$utils/gsaphome';
 
+// Create new link Element
+var linkrel = document.createElement('link');
+
+// set the attributes for link element
+linkrel.rel = 'stylesheet';
+
+linkrel.type = 'text/css';
+
+linkrel.ref = 'http://localhost:3000/swiperhome.css';
+
 window.Webflow ||= [];
 window.Webflow.push(() => {
   $('.tab-link').hover(function () {
