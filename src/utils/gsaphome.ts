d22eb90e17f4f0463455d81e9d33370bc2cd5751
adammuflihun <96274551+adammuflihun@users.noreply.github.com>
2022-09-09 13:42:55 +0700

    s

--- src/utils/gsaphome.ts
+++ src/utils/gsaphome.ts
@@ -1,5 +1,7 @@
 import gsap from 'gsap';
 
+gsap.registerPlugin(ScrollTrigger);
+
 export const gsaphome = () => {
   console.log(`ancol!`);
 
@@ -27,4 +29,21 @@ export const gsaphome = () => {
     })
     .from('.graphic-box', { y: 150, opacity: 0 }, 0.5)
     .to('.graphic-box', { duration: 2, yoyo: 1, y: 50, repeat: -1, ease: 'Power1.easeInOut' }, 1);
+
+  gsap.from('.wrap-boxes', {
+    duration: 10,
+    x: '-50vw',
+    ease: 'linear',
+    scrub: 1,
+    scrollTrigger: {
+      trigger: '.section-solutions',
+      markers: true,
+      start: 'top 75%', //when top of herman passes 75% viewport height
+      end: 'bottom 25%', //when bottom of herman passes 25% viewport height
+
+      //events: onEnter onLeave onEnterBack onLeaveBack
+      toggleActions: 'restart complete reverse reset',
+      //options: play, pause, resume, reset, restart, complete, reverse,none
+    },
+  });
 };
